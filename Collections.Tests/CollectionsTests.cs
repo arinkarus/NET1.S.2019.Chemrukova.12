03702430e9ba4598a13cde2c46df122482d6add0
arinkarus <arinkarus@gmail.com>
2019-04-17 02:05:50 +0300

    Added CheckIfParenthesisAreBalanced and GetPrimeNumbers

---
+++ Collections.Tests/CollectionsTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Collections.Tests
+{
+    public class CollectionsTests
+    {
+        [Test]
+        public void GetPrimeNumbers_LimitIsNegative_ThrowArgumentException() =>
+             Assert.Throws<ArgumentException>(() => CollectionsMethods.Collections.GetPrimeNumbers(-5));
+
+        [TestCase(200, ExpectedResult = new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199 })]
+        [TestCase(2, ExpectedResult = new int[] { 2 })]
+        [TestCase(15, ExpectedResult = new int[] { 2, 3, 5, 7, 11, 13 })]
+        public int[] GetPrimeNumbers_LimitIsGiven_ReturnPrimeNumbers(int limit)
+        {
+            return CollectionsMethods.Collections.GetPrimeNumbers(limit).ToArray();
+        }
+
+        [Test]
+        public void CheckIfParenthesisAreBalanced_InputStringIsNull_ThrowArgumentNullException() =>
+          Assert.Throws<ArgumentNullException>(() => CollectionsMethods.Collections.CheckIfParenthesisAreBalanced(null));
+
+        [TestCase("", ExpectedResult = true)]
+        [TestCase("[288 votes so far. Categories: {Everything Else (47 votes), C# (61 votes), C++ (39 votes), Database (44 votes), Mobile (45 votes), Web Dev (52 votes)}]", ExpectedResult = true)]
+        [TestCase("[{}()<sometext>[[{{}}]]]", ExpectedResult = true)]
+        [TestCase("[ ( ] )", ExpectedResult = false)]
+        [TestCase("(x+8)*[((some)]", ExpectedResult = false)]
+        [TestCase("(x+8)*[(some)]", ExpectedResult = true)]
+        public bool CheckIfParenthesisAreBalanced_InputString_ReturnResult(string input)
+        {
+            return CollectionsMethods.Collections.CheckIfParenthesisAreBalanced(input);
+        }
+
+    }
+}
---
+++ Collections/Collections.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CollectionsMethods
+{
+    /// <summary>
+    /// Contains methods that works with collections.
+    /// </summary>
+    public static class Collections
+    {
+        /// <summary>
+        /// Returns prime numbers that are below limit.
+        /// </summary>
+        /// <param name="limit">Given limit.</param>
+        /// <returns>Found prime numbers.</returns>
+        public static IEnumerable<int> GetPrimeNumbers(int limit)
+        {
+            CheckIfPositiveNumber(limit);
+            return SieveOfEratosthenes(limit);
+
+            IEnumerable<int> SieveOfEratosthenes(int upperLimit)
+            {
+                int sieveBound = (upperLimit - 1) / 2;
+                int upperSqrt = ((int)Math.Sqrt(upperLimit) - 1) / 2;
+                BitArray bits = new BitArray(sieveBound + 1, true);
+                yield return 2;
+                for (int i = 1; i <= upperSqrt; i++)
+                {
+                    if (bits.Get(i))
+                    {
+                        yield return 2 * i + 1;
+                        for (int j = i * 2 * (i + 1); j <= sieveBound; j += 2 * i + 1)
+                        {
+                            bits.Set(j, false);
+                        }
+                    }
+                }
+           
+                for (int i = upperSqrt + 1; i <= sieveBound; i++)
+                {
+                    if (bits.Get(i))
+                    {
+                        yield return 2 * i + 1;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks if brackets are balanced.
+        /// </summary>
+        /// <param name="input">Given string.</param>
+        /// <returns>True if parenthesis are balanced.</returns>
+        public static bool CheckIfParenthesisAreBalanced(string input)
+        {
+            input.CheckOnNull();
+            Dictionary<char, char> dictionary = GetParenthesisDictionary();
+            var stack = new Stack<char>();
+            foreach (var symbol in input)
+            {
+                if (dictionary.Values.Contains(symbol))
+                {
+                    stack.Push(symbol);
+                }
+                else
+                {
+                   if (dictionary.Keys.Contains(symbol))
+                   {
+                       char openingBracket = stack.Peek();
+                       if (!(openingBracket == dictionary[symbol]))
+                       {
+                            return false;
+                       }
+
+                       stack.Pop();
+                   }             
+                }
+            }
+
+            return true;
+        }
+
+        private static bool Contains(this IEnumerable<char> openingParenthesis, char symbol)
+        {
+            foreach (var bracket in openingParenthesis)
+            {
+                if (symbol == bracket)
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        private static Dictionary<char, char> GetParenthesisDictionary() {
+            return 
+            new Dictionary<char, char>
+            {
+                [')'] = '(',
+                [']'] = '[',
+                ['}'] = '{',
+            };
+        }
+
+        private static void CheckIfPositiveNumber(int number)
+        {
+            if (number <= 0)
+            {
+                throw new ArgumentException($"{nameof(number)} has to be greater than 1.");
+            }
+        }
+
+        private static void CheckOnNull(this object obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException($"{nameof(obj)} can't be null!");
+            }
+        }
+    }
+}
